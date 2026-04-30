0d6edb030d3f77b6fb5017ea95fa9f19a7859422
SlatherEss <slathercordova@gmail.com>
2026-04-30 17:04:32 -0500

    Initial commit

---
+++ person-service/src/main/java/com/condominio/PersonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PersonServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonServiceApplication.class, args);
+    }
+}
