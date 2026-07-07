e26b69daf8cbea6f72fb7f92c7b9963306848309
SlatherEss <slathercordova@gmail.com>
2026-07-07 15:15:56 -0500

    se agrega el id de la unidad al response

--- edificio-service/src/main/java/com/condominio/edificio/edificio/dto/response/UnidadDetailResponse.java
+++ edificio-service/src/main/java/com/condominio/edificio/edificio/dto/response/UnidadDetailResponse.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UnidadDetailResponse {
     private UUID idEdificio;
+    private UUID id;
     private String codigo;
     private String logoUrl;
     private Integer piso;
